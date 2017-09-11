859a10c84d9301a8954ac65151f0393431a9b334
mohamedElsonny <mohamedelsonny5@gmail.com>
2017-09-11 21:28:54 +0200

    Add GEt /todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Library Imports
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 
 // Local Imports
@@ -31,6 +32,22 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id)
+        .then((todo) => {
+            if (!todo) {
+                return res.send();
+            }
+            res.send({ todo });
+        }).catch(e => res.status(404).send());
 });
 
 
