a349c87d2713a647195efa960828d2d5b8b70356
mohamedElsonny <mohamedelsonny5@gmail.com>
2017-09-11 15:45:05 +0200

    Add GET route

--- server/server.js
+++ server/server.js
@@ -25,6 +25,15 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Starting on Port 3000');
