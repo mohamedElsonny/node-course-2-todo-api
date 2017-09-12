b2e56930aa00597955455fc4ed9cacc3d617e90d
mohamedElsonny <mohamedelsonny5@gmail.com>
2017-09-12 06:21:07 +0200

    Test the DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -104,3 +104,47 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+
+describe('DELETE /todos/:id', () => {
+
+    it('should delete todo by id and return the todo', (done) => {
+        let hexId = todos[0]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId)
+                    .then((res) => {
+                        expect(res).toNotExist();
+                        done();
+                    }).catch(err => done(err));
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        let hexId = new ObjectID().toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if id is not valid', (done) => {
+        let hexId = '123';
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+});
