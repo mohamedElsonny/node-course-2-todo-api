2d921df806cf34fe6f9d8cc660cfbba910d2cf59
mohamedElsonny <mohamedelsonny5@gmail.com>
2017-09-15 23:50:28 +0200

    Add tests for DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -158,4 +158,4 @@ app.listen(port, () => {
 });
 
 
-module.exports = { app };
+module.exports = { app }; 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -366,3 +366,29 @@ describe('POST /users/login', () => {
             });
     });
 });
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        let email = users[0].email;
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+
+                User.findOne({ email })
+                    .then((user) => {
+                        if (!user) {
+                            return done(err);
+                        }
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    })
+                    .catch(e => done(e));
+            });
+    });
+});
